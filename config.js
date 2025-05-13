0b08bef32880c7ad3ac8eb7b38aa59d6bceb4e04
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-05-13 18:13:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,19 +21,20 @@
 
 const mysql = require('mysql2/promise');
 
-// Update the connection details with your URL
+// Update the connection details with the new URL and credentials
 const db = mysql.createPool({
-    host: 'gondola.proxy.rlwy.net',  // Use the provided host URL
-    port: 30796,                     // Use the provided port
-    user: 'root',                    // Your MySQL username
-    password: 'EtshGFvMuFoUkIcswQouzTAYNxISRJzM',  // Your MySQL password
-    database: 'railway',             // Use the provided database name
-    multipleStatements: true        // Allow multiple statements if needed
+    host: 'centerbeam.proxy.rlwy.net',  // Updated host URL
+    port: 47231,                        // Updated port
+    user: 'root',                       // MySQL username
+    password: 'zWTtpPmGIfrZBZXiIlIxtliJIJwnANRi',  // Updated password
+    database: 'railway',                // Updated database name
+    multipleStatements: true           // Allow multiple statements if needed
 });
 
 console.warn('Connected');
 
 module.exports = db;
+
 
 
 
