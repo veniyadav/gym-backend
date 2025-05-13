6892454b4a009a1ad13595793b6b16ed28359ac5
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-05-13 16:31:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,39 @@
-const mysql = require('mysql2/promise');
+// local server
 
-const db = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'gym',
-    multipleStatements: true
-});
+//const mysql = require('mysql2/promise');
+
+//const db = mysql.createPool({
+//    host: '127.0.0.1',
+//    port: 3306,
+//    user: 'root',
+//    password: '',
+//    database: 'gym',
+//    multipleStatements: true
+//});
 
  
+
+//console.warn('Connected');
+
+//module.exports = db;
+
+// live server
+
+const mysql = require('mysql2/promise');
+
+// Update the connection details with your URL
+const db = mysql.createPool({
+    host: 'gondola.proxy.rlwy.net',  // Use the provided host URL
+    port: 30796,                     // Use the provided port
+    user: 'root',                    // Your MySQL username
+    password: 'EtshGFvMuFoUkIcswQouzTAYNxISRJzM',  // Your MySQL password
+    database: 'railway',             // Use the provided database name
+    multipleStatements: true        // Allow multiple statements if needed
+});
 
 console.warn('Connected');
 
 module.exports = db;
+
 
 
